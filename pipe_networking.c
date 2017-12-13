fbea5417fb98b4094fb57c18974aba7bdf22bca9
saltonbench <jlin22@stuy.edu>
2017-12-13 11:47:26 -0500

    working on it

--- pipe_networking.c
+++ pipe_networking.c
@@ -10,12 +10,23 @@
   returns the file descriptor for the upstream pipe.
   =========================*/
 int server_handshake(int *to_client) {
-  //to_client is the fd for downstream
+  //c to s upstream
+  //s to c downstream
+  //wkp is upstream
+  
   char * path = "wkp";
   mkfifo(path,0644);
-  int fd = open(path, O_WRONLY);
+  int up = open(path, O_WRONLY);
+  /*write(up, &up , sizeof(int));
+  close(up);*/
+  //to client is downstream
+  int down=(&to_client, O_RDONLY);
+  /*int value;
+  read(down, &value, sizeof(int));
+  close(down);*/
+  to_client = &down;
   
-  return fd;
+  return up;
 }
 
 
@@ -29,9 +40,16 @@ int server_handshake(int *to_client) {
   returns the file descriptor for the downstream pipe.
   =========================*/
 int client_handshake(int *to_server) {
+  //private is downstream
   char * path = "private";
   mkfifo(path,0644);
-  int fd = open(path, O_WRONLY);
-  
-  return fd;
+  int down = open(path, O_WRONLY);
+  /*read(down, &down, sizeof(int));
+    close(down);*/
+  int up=(&to_server, O_RDONLY);
+  /*int value;
+  read(up, &value, sizeof(int));
+  close(up);*/
+  to_server = &up;
+  return down;
 }
